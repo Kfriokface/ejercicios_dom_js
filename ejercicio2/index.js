01ad3df6cb70fce6fb89987afe3802e81c04f335
Alberto Sancho <99506259+Kfriokface@users.noreply.github.com>
2024-08-08 12:15:55 +0200

    ejercicio 2

--- ejercicio2/index.js
+++ ejercicio2/index.js
@@ -23,3 +23,21 @@ pDinamico.appendChild(textoDinamico);
 document.body.appendChild(pDinamico);
 
 // 2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
+let wubba = document.querySelector("h2.fn-insert-here");
+wubba.innerHTML= "Wubba Lubba dub dub";
+
+// 2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+let ul = document.createElement("ul");
+document.body.appendChild(ul);
+for (i = 0; i < apps.length; i++) {
+  let li = document.createElement("li");
+  li.innerHTML = apps[i];
+  ul.appendChild(li);
+}
+
+// 2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+let itemsToRemove = document.querySelectorAll(".fn-remove-me");
+itemsToRemove.forEach(item => {
+  item.remove();
+});
