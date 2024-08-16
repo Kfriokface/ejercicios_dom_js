147bdf9ff66e7e5c9d5c6b476de5c889f4406a58
Alberto Sancho <99506259+Kfriokface@users.noreply.github.com>
2024-08-16 23:16:27 +0200

    ejercicio 4

---
+++ ejercicio4/index.js
@@ -0,0 +1,28 @@
+// 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
+// evento click que ejecute un console log con la información del evento del click
+const newButton = document.createElement('button');
+newButton.textContent = 'Click me!';
+newButton.setAttribute('id', 'btnToClick');
+document.body.appendChild(newButton);
+let showEventInfo = function(event) {
+    console.log(event);
+};
+document.getElementById("btnToClick").addEventListener("click", showEventInfo);
+
+// 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
+let focusInputValue =  function() {
+  console.log('Valor del input (focus): ' + this.value);
+};
+document.querySelector('.focus').addEventListener('focus', focusInputValue);
+
+// 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
+
+// Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html que imprima cada uno de los albums.
+
+const albums = [
+  "De Mysteriis Dom Sathanas",
+  "Reign of Blood",
+  "Ride the Lightning",
+  "Painkiller",
+  "Iron Fist",
+];
